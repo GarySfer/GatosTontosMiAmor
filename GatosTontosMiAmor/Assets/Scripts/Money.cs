acb866b84e207eadbc5810356d2e2475a74a53a9
JakubKapica <u36_jakkap_waw@technischools.com>
2023-10-24 15:12:22 +0200

    kasa kasa kasa

--- GatosTontosMiAmor/Assets/Scripts/Money.cs
+++ GatosTontosMiAmor/Assets/Scripts/Money.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int Money;
+
+    void Start() {
+        // Money = startingMoney;
+    }
+    
+    void Update() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void addMoney(int addToMoney) {
+        Money += addToMoney;
     }
+
+    public void subtractMoney(int subtractToMoney) {
+        if(Money -= subtractToMoney < 0) {
+            Debug.Log("Nie wystarczająco pieniążków");
+        } else {
+            Money -= subtractToMoney;
+        }
+    } 
 }
