ef3ca0210516424e67840c592a14ea8e0d31a967
JakubKapica <u36_jakkap_waw@technischools.com>
2023-11-03 14:02:31 +0100

    kasa działa

--- GatosTontosMiAmor/Assets/Scripts/Money.cs
+++ GatosTontosMiAmor/Assets/Scripts/Money.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Money : MonoBehaviour
 {
-    public int Money;
-
+    public int money;
+    public TMP_Text moneyBankAccount;
+    public int startingMoney = 0;
     void Start() {
-        // Money = startingMoney;
+        addMoney(startingMoney);
+        refreshMoney(money);
     }
     
     void Update() {
-        
+
+    }
+
+    public void refreshMoney(int refreshMoney) {
+        moneyBankAccount.text = money.ToString();
     }
 
     public void addMoney(int addToMoney) {
-        Money += addToMoney;
+        money += addToMoney;
     }
 
     public void subtractMoney(int subtractToMoney) {
-        if(Money -= subtractToMoney < 0) {
+        if(money - subtractToMoney < 0) {
             Debug.Log("Nie wystarczająco pieniążków");
         } else {
-            Money -= subtractToMoney;
+            money -= subtractToMoney;
         }
     } 
 }
